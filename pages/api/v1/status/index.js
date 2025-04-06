9878fec54d86a33d2ab1c787bcefb86c668c194e
Igor de Andrade <cadastros.igordeandrade@gmail.com>
2025-04-06 19:39:00 +0000

    finaliza o endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,29 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int AS open_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConnections =
+    dbOpenedConnectionsResult.rows[0].open_connections;
+
   response.status(200).json({
-    mensagem: "alunos do curso.dev são pessoas acima da média",
+    updated_at: updatedAt,
+    database: {
+      max_connections: parseInt(dbMaxConnections),
+      opened_connections: dbOpenedConnections,
+      version: dbVersion,
+    },
   });
 }
 
