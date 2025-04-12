3d3c684833d23aaaffe0d7c54d71c6ad051cbbdc
Igor de Andrade <cadastros.igordeandrade@gmail.com>
2025-04-12 19:29:24 +0000

    adicionada o endpoint `/api/v1/migrations`

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,28 @@
+import database from "infra/database.js";
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+beforeAll(cleanDatabase);
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(201);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const newResponse = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+
+  expect(newResponse.status).toBe(200);
+  const newResponseBody = await newResponse.json();
+
+  expect(Array.isArray(newResponseBody)).toBe(true);
+});
